cf83c064b156020643e18c1da1fecf7b02fcd3f0
ArceusOmkar7 <omkarmahindrakar24@gmail.com>
2025-02-07 14:27:25 +0530

    fixed votes endpoint

--- app/api/votes/route.ts
+++ app/api/votes/route.ts
@@ -1,30 +1,67 @@
+"use client";
+
 import prisma from "@/app/lib/prisma";
-import { NextRequest, NextResponse } from "next/server"
+import { useSearchParams } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request){
+export async function GET(req: Request) {
+  const searchParams = useSearchParams();
+  const fetchedId = searchParams.get("proposals_fetchedId");
 
-    // const allVoters = await prisma.votes.findMany();
-    return NextResponse.json("Working On")
+  if (!fetchedId) {
+    return NextResponse.json(
+      {
+        error: "Invalid proposal id",
+      },
+      { status: 401 }
+    );
+  }
+  try {
+    const id = parseInt(fetchedId);
+
+    const votes = await prisma.votes.findMany({
+      where: {
+        proposal_id: id,
+      },
+    });
+    const totalVotes = votes.length;
+    votes.filter((vote) => {
+      if (vote.vote === "upvote") return vote;
+    });
+
+    return NextResponse.json({
+      upvotes: votes.length,
+      downvote: totalVotes - votes.length,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Cannot find votes",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
+export async function POST(req: NextRequest) {
+  const { user_id, proposal_id, vote } = await req.json();
 
-export async function POST(req : NextRequest){
-    const {user_id , proposal_id, vote} = await req.json();
+  try {
+    const CreateVote = await prisma.votes.create({
+      data: {
+        user_id,
+        proposal_id,
+        vote,
+      },
+    });
 
-    try{
-        const CreateVote = await prisma.votes.create({
-            data: {
-                user_id, 
-                proposal_id,
-                vote
-            }
-
-        });
-
-        return NextResponse.json( CreateVote,{ status: 200 });
-    }
-    catch(error){
-        return NextResponse.json({ error: "Failed to create Vote" },{ status: 401 })
-    }
+    return NextResponse.json(CreateVote, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create Vote" },
+      { status: 401 }
+    );
+  }
 }
-
